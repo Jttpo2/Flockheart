917eca7b9610683eaa16db9146c014b042849fde
John Petersson <johnpetersson@gmail.com>
2016-11-26 02:21:01 +0100

    Using unity vel/acc

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,13 +4,14 @@ using System.Collections;
 public class Mover : MonoBehaviour
 {
 
-	private Transform body;
+	private Transform bodyTransform;
+	private Rigidbody body;
 
 	private Vector3 pos;
 	private Vector3 vel;
 	private Vector3 acc;
 
-	private Vector3 initialVelocity = new Vector3 (0.2f, 0.0f, 0.0f);
+	private Vector3 initialVelocity = new Vector3 (10.0f, 0.0f, 0.0f);
 
 	public float topSpeed = 10;
 
@@ -18,21 +19,31 @@ public class Mover : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
-		body = this.transform;
-		pos = body.transform.position;
-		vel = initialVelocity;
+		bodyTransform = this.transform;
+		body = this.GetComponent <Rigidbody> ();
+
+//		pos = body.position;
+//		vel = initialVelocity;
+//		acc = Vector3.zero;
+
+		body.velocity = initialVelocity;
 		acc = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate ()
 	{
-		vel += acc;
-		vel = Vector3.ClampMagnitude (vel, topSpeed); // Limit velocity
+//		vel += acc;
+//		vel = Vector3.ClampMagnitude (vel, topSpeed); // Limit velocity
+//		pos += vel;
 
-		pos += vel;
+		body.AddForce (acc);
+		body.velocity = Vector3.ClampMagnitude (body.velocity, topSpeed); // Limit velocity
 
-		body.position = pos;
+
+
+
+//		bodyTransform.position = pos;
 //		body.GetComponent <Rigidbody> ().AddForce (acc);
 	}
 
