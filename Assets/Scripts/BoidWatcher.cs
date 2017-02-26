1911e170ac0d698a16c3ba6e0c429a3110656317
John Petersson <johnpetersson@gmail.com>
2017-02-26 16:33:59 +0100

    Flock cam pos

--- Assets/Scripts/BoidWatcher.cs
+++ Assets/Scripts/BoidWatcher.cs
@@ -21,16 +21,10 @@ public class BoidWatcher : MonoBehaviour
 	void LateUpdate ()
 	{
 		if (boidController) {
-//			float desiredAngle = target.transform.eulerAngles.y;
-//			Quaternion rotation = Quaternion.Euler (0, desiredAngle, 0);
-
 			targetPos = boidController.GetComponent <BoidController> ().getFlockCenter ();
 			transform.position = targetPos - offset;
 
 			transform.LookAt (targetPos);
-
-//			Vector3 watchPoint = boidController.GetComponent<BoidController> ().getFlockCenter ();
-//			transform.LookAt (watchPoint + boidController.transform.position);
 		}
 	}
 }
