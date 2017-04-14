59b85f7fca62bca5bf3c120cf3fd72fa51adfc89
John Petersson <johnpetersson@gmail.com>
2017-04-14 04:07:19 +0200

    Some kind of zoom added on mouse button 0 hold-down

--- Assets/Scripts/BoidWatcher.cs
+++ Assets/Scripts/BoidWatcher.cs
@@ -8,9 +8,16 @@ public class BoidWatcher : MonoBehaviour
 	private Vector3 targetPos;
 
 	private Vector3 offset;
+	private Vector3 mouseOrigin;
+	private Vector3 prevMousePos;
+	private bool isPanning;
+
+	private float zoomSpeed = 0.01f;
 
 	void Start ()
 	{
+		prevMousePos = Vector3.zero;
+
 		if (boidController) {
 			targetPos = boidController.GetComponent <BoidController> ().getFlockCenter ();
 			offset = targetPos - transform.position;
@@ -20,11 +27,37 @@ public class BoidWatcher : MonoBehaviour
 
 	void LateUpdate ()
 	{
+		if (Input.GetMouseButtonDown(0)) {
+			prevMousePos = Input.mousePosition;
+			isPanning = true;
+		} 
+		if (Input.GetMouseButtonUp(0)) {
+			isPanning = false;
+		}
+		if (isPanning) {
+			Vector3 newPos = Input.mousePosition - prevMousePos;
+			newPos.z *= 0; // Don't change z axis
+			newPos.x *= zoomSpeed;
+			newPos.y *= zoomSpeed;
+			offset -= newPos;
+
+			prevMousePos = Input.mousePosition;
+		}
+
 		if (boidController) {
 			targetPos = boidController.GetComponent <BoidController> ().getFlockCenter ();
 			transform.position = targetPos - offset;
 
+
 			transform.LookAt (targetPos);
 		}
 	}
+
+//	void zoomIn() {
+//		offset *= 1.0f + zoomSpeed;
+//	}
+//
+//	void zoomOut() {
+//		offset *= 1.0f - zoomSpeed;
+//	}
 }
