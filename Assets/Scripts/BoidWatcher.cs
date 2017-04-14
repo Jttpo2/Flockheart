fa7d7c1ad072cd6bf7a9d33da75611d7a631a73d
John Petersson <johnpetersson@gmail.com>
2017-04-15 01:39:24 +0200

    Revert "Trying to constrain camera tilt so it does not go beyond straight up or down"
    This reverts commit 0c51d142348ec0fd48d300b1ee6abbb86900696f.

---
+++ Assets/Scripts/BoidWatcher.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidWatcher : MonoBehaviour
+{
+	public Transform boidController;
+
+	private Vector3 targetPos;
+
+	private Vector3 offset;
+	private Vector3 mouseOrigin;
+	private Vector3 prevMousePos;
+	private bool isPanning;
+
+	private float rotationSpeedX = 0.2f;
+	private float rotationSpeedY = 0.2f;
+
+	void Start ()
+	{
+		prevMousePos = Vector3.zero;
+
+		if (boidController) {
+			targetPos = boidController.GetComponent <BoidController> ().getFlockCenter ();
+			offset = targetPos - transform.position;
+		}
+	}
+
+	void LateUpdate ()
+	{
+		if (boidController) {
+			targetPos = boidController.GetComponent <BoidController> ().getFlockCenter ();
+			transform.position = targetPos - offset;
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			prevMousePos = Input.mousePosition;
+			isPanning = true;
+		} 
+		if (Input.GetMouseButtonUp(0)) {
+			isPanning = false;
+		}
+		if (isPanning) {
+			Vector3 mouseDelta = Input.mousePosition - prevMousePos;
+			rotateAroundFlock (mouseDelta.x * rotationSpeedX);
+
+			panUpAndDown (mouseDelta.y * rotationSpeedY);
+
+			// Update offset
+			offset = targetPos - transform.position;
+
+			prevMousePos = Input.mousePosition;
+		}
+			
+		transform.LookAt (targetPos);
+	}
+
+	void rotateAroundFlock(float degrees) {
+		transform.RotateAround (targetPos, Vector3.up, degrees);
+	}
+
+	void panUpAndDown(float degrees) {
+		Vector3 horizontalAndPerpendicularToCamView = Vector3.Cross(targetPos - transform.position, Vector3.up);
+		transform.RotateAround (targetPos, horizontalAndPerpendicularToCamView, degrees);
+	}
+}
