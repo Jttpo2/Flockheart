a8b43b7c6551f693ee04b46eb67116d8a585288e
John Petersson <johnpetersson@gmail.com>
2017-04-16 03:45:51 +0200

    Fading stuff between camera and selected objects instead of everything within a set distance

--- Assets/Scripts/ClearSight.cs
+++ Assets/Scripts/ClearSight.cs
@@ -2,38 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Modified: http://answers.unity3d.com/questions/44815/make-object-transparent-when-between-camera-and-pl.html
 public class ClearSight : MonoBehaviour
 {
-	public float DistanceToPlayer = 5.0f;
 	public Material TransparentMaterial = null;
 	public float FadeInTimeout = 0.6f;
 	public float FadeOutTimeout = 0.2f;
 	public float TargetTransparency = 0.3f;
+
+	// The objects to keep in view (fade everyting between them and the camera - that is in the correct layer mask)
+	public List<Transform> clearObjectsList;
 
 	// Layer mask to only hit desired objects
 	public LayerMask layerMask;
 
 	private void Update ()
 	{
-		RaycastHit[] hits; // you can also use CapsuleCastAll() 
-		hits = Physics.RaycastAll (transform.position, transform.forward, DistanceToPlayer, layerMask);
-		foreach (RaycastHit hit in hits) {
-			Debug.DrawLine (transform.position, hit.point, Color.green);
-			Renderer R = hit.collider.GetComponent<Renderer> ();
-			if (R == null) {
-				continue;
+		foreach (Transform viewObject in clearObjectsList) {
+			RaycastHit lineHit = new RaycastHit ();
+			if (Physics.Linecast (transform.position, viewObject.position, out lineHit, layerMask)) {
+//				Debug.DrawLine (transform.position, lineHit.point, Color.cyan);
+				Renderer R = lineHit.collider.GetComponent<Renderer> ();
+				if (R == null) {
+					continue;
+				}
+				makeTransparent (R);
 			}
-			// no renderer attached? go to next hit 
-			// TODO: maybe implement here a check for GOs that should not be affected like the player
-			AutoTransparent AT = R.GetComponent<AutoTransparent> ();
-			if (AT == null) { // if no script is attached, attach one
-				AT = R.gameObject.AddComponent<AutoTransparent> ();
-				AT.TransparentMaterial = TransparentMaterial;
-				AT.FadeInTimeout = FadeInTimeout;
-				AT.FadeOutTimeout = FadeOutTimeout;
-				AT.TargetTransparency = TargetTransparency;
-			}
-			AT.BeTransparent (); // get called every frame to reset the falloff
 		}
+	}
+
+	private void makeTransparent (Renderer R)
+	{
+		AutoTransparent AT = R.GetComponent<AutoTransparent> ();
+		if (AT == null) { // if no script is attached, attach one
+			AT = R.gameObject.AddComponent<AutoTransparent> ();
+			AT.TransparentMaterial = TransparentMaterial;
+			AT.FadeInTimeout = FadeInTimeout;
+			AT.FadeOutTimeout = FadeOutTimeout;
+			AT.TargetTransparency = TargetTransparency;
+		}
+		AT.BeTransparent (); // get called every frame to reset the falloff
 	}
 }
